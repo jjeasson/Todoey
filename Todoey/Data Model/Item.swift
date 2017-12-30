394830844d06dceda8c5545798aba3899bd9516c
Joseph J Easson <josephj.easson@gmail.com>
2017-12-30 15:35:22 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Joseph J Easson on 30/12/2017.
+//  Copyright © 2017 Joseph J Easson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
